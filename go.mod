03a705d23b5bcdd2c5b30a7bf1a6de42cd15f92e
anchor54 <ankur.mazumder54@gmail.com>
2025-11-11 23:22:14 +0530

    init repo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module redis-sync
+
+go 1.25.3
+
+require golang.org/x/sys v0.38.0 // indirect
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+)
+
+
+/**
+	Commands to support:
+		- [ ] add data
+		- [ ] start resync process
+			- [ ] create child process
+			- [ ] provides a private copy of the in memory data
+			- 
+
+*/
+
+func main() {
+	fmt.Println("Hello, World!")
+}
