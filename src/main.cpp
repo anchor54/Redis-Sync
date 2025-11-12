903c41c54df7faa89d00f337596ab2d077ca6658
anchor54 <ankur.mazumder54@gmail.com>
2025-11-12 22:42:44 +0530

    added fork and async reap functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <functional>
+#include <functional>   // for function templates
+#include <unistd.h>     // for fork
+#include <sys/wait.h>   // for wait
+#include <signal.h>
 
 #include "utils.h"
 
 /**
  * - [x] Keep accepting requests from terminal
  * - [ ] On BGSAVE:
- *      - [ ] parent forks a child process
+ *      - [x] parent forks a child process
  *      - [ ] parent start writting newer data to circular backup buffer
  *      - [ ] the child process writes all the data to RDB file
  *      - [ ] sends the RDB file over network to replica
@@ -43,19 +46,41 @@ void setHandler(std::vector<std::string> args) {
     std::cout << "Done!" << std::endl;
 }
 
+void bgsaveHandler(std::vector<std::string> args) {
+    pid_t pid = fork();
+    if (pid == 0) {      // <-- this is a child process
+        std::cout << "Starting background save... \n";
+        sleep(5);
+        std::cout << "Background save done...\n";
+        std::cout << "Child Exiting...\n";
+        exit(0);
+    }
+}
+
 void defaultHandler(std::string &userInput) {
     std::cout << "[User Input]: " << userInput << std::endl;
 }
 
+void sigchild_handler(int sig) {
+    int status;
+    for (int child_pid = waitpid(-1, &status, WNOHANG); child_pid > 0; child_pid = waitpid(-1, &status, WNOHANG)) {
+        std::cout << "Reaping child: " << child_pid << std::endl;
+    }
+}
+
 int main() {
+    signal(SIGCHLD, sigchild_handler);  // reaps the child asynchronously
+
     while (true) {
         std::string input;
+        std::cout << "What do you wanna run today?: ";
         std::getline(std::cin, input);
         
         std::map<std::string, handlerFn> handlerMap {
             { "exit", exitHandler },
             { "get", getHandler },
             { "set", setHandler },
+            { "bgsave", bgsaveHandler },
         };
         
         std::vector<std::string> tokens = split(input, ' ');
@@ -68,5 +93,7 @@ int main() {
         } else {
             defaultHandler(input);
         }
+
+        std::cout << "--------------------------------------------\n";
     }
 }
